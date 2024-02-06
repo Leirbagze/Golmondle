c69de1a939030a404c56b164d94d030c4445ac63
Yan <yan.hourdebaigt.hiroux@gmail.com>
2024-02-06 15:58:04 +0100

    barre de feur

--- golmondle.js
+++ golmondle.js
@@ -1,16 +1,23 @@
+function cacherLigne(golnom) {
+    let ligneACacher = document.getElementById(golnom);
+    ligneACacher.style.display = "none";
+}
+
+function afficherLigne(golnom) {
+    let ligneAVisible = document.getElementById(golnom);
+    ligneAVisible.style.display = "table-row";
+}
+
 function verifierTouche(event) {
     if (event.key === "Enter") {
-        var valeurEntree = document.getElementById('entree').value;
-        alert("La valeur de l'input est : " + valeurEntree);
+        let valeurEntree = document.getElementById("entree").value;
+        let tableauNoms = Array.from(document.getElementsByClassName("golnom"));
+        let nomTrouve = tableauNoms.some(function(balise) {
+            return balise.textContent === valeurEntree;
+        });
+        if (nomTrouve){
+            afficherLigne(valeurEntree);
+            document.getElementById("entree").value = "";
+        }
     }
-}
-
-function cacherLigne() {
-    var ligneACacher = document.getElementsByClassName('cacher')[0];
-    ligneACacher.style.display = 'none';
-}
-
-function afficherLigne() {
-    var ligneAVisible = document.getElementsByClassName('visible')[0];
-    ligneAVisible.style.display = 'table-row';
 }
