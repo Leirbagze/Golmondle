066839411e69b1010f4db654b7724fef22dbe7f3
Gabriel PRETOU <pretougabr@cy-tech.fr>
2024-02-07 12:44:00 +0100

    f

--- golmondle.js
+++ golmondle.js
@@ -9,10 +9,20 @@ function afficherLigne(golnom) {
 }
 
 function verifierTouche(event) {
+    let valeurEntree = document.getElementById("entree").value;
+    let tableauGolmons = Array.from(document.getElementsByClassName("golmon"));
+    let i=valeurEntree.length;
+    let test=false;
+    for (let k=0;k<10;k++){
+        if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {
+            if (event.key===tableauGolmons[k].id[i]){
+                test=true;
+                alert("test");
+            }
+        }
+    }
     if (event.key === "Enter") {
-        let valeurEntree = document.getElementById("entree").value;
-        let tableauNoms = Array.from(document.getElementsByTagName("tr"));
-        let nomTrouve = tableauNoms.some(function(balise) {
+        let nomTrouve = tableauGolmons.some(function(balise) {
             return balise.id === valeurEntree;
         });
         if (nomTrouve){
